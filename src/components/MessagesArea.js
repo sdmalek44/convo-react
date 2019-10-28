361b6eb80eba84dc0a663043ceb9729f95dd69c2
sdmalek44 <malek.stephen44@gmail.com>
2019-10-27 19:03:55 -0600

    Add MessagesArea component

---
+++ src/components/MessagesArea.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import NewMessageForm from './NewMessageForm';
+
+const MessageAre = ({ conversation: { id, title, messages } }) => {
+  return (
+    <div className="messagesArea">
+      <h2>{title}</h2>
+      <ul>{orderedMessages(messages)}</ul>
+      <NewMessageForm conversation_id={id} />
+    </div>
+  )
+}
+
+export default MessagesArea;
+
+const orderedMessages = messages => {
+  const sortedMessages = messages.sort(
+    (a, b) => new Date(a.created_at) - new Date(b.created_at)
+  );
+
+  return sortedMessages.map(message => {
+    return <li key={message.id}>{message.text}</li>
+  });
+};
